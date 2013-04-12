008fa9a4249ac88f7b99457228428330c4bd7d1c
Hang Chen <chsc4698@gmail.com>
2013-04-12 16:06:25 +0800

    clean

--- src/common.c
+++ src/common.c
@@ -5,22 +5,17 @@ int n_round,
     n_block = 4,
     n_key;
 
-uint t_keys[60];
-
 uint round_keys[60 * 4];
 
 #define MAES_rotate_left_m(x) (x << 8 | x >> 24)
 #define MAES_sub_word_m(x) (sbox[x >> 24] << 24        | sbox[(x >> 16) & 0xff] << 16 |\
                             sbox[(x >> 8) & 0xff] << 8 | sbox[x & 0xff])
-#define MAES_inv_sub_word_m(x) (inv_sbox[x >> 24] << 24        | inv_sbox[(x >> 16) & 0xff] << 16 |\
-                                inv_sbox[(x >> 8) & 0xff] << 8 | inv_sbox[x & 0xff])
-#define MAES_shift_row_m(w1, w2, w3, w4) ((w1 & 0xff000000) | (w2 & 0x00ff0000) |\
-                                          (w3 & 0x0000ff00) | (w4 & 0x000000ff))
+
 void
 MAES_key_schedule(uint key[])
 {
     int i;
-    uint temp;
+    uint temp, t_keys[60];
 
     for (i = 0; i < n_key; t_keys[i] = key[i], ++i)
         ;
